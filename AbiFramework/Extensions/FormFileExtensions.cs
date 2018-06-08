d61227696b7eec8239a8b3b0d362157cfd7801e8
Abiezer Matos <abiezerm@gmail.com>
2018-06-08 16:13:45 -0400

    added FormFile Extensions class

---
+++ AbiFramework/Extensions/FormFileExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace AbiFramework.Extensions
+{
+    public static class FormFileExtensions
+    {
+        public static byte[] ReadAllAsByte(this IFormFile formFile)
+        {
+            using (var ms = new MemoryStream())
+            {
+                formFile.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
